c747d64cc5964351a6f410fd8a58763de1396af9
Marcus Jackson <jacks111@cougars.csusm.edu>
2019-03-15 18:23:47 -0700

    Checking if changes occur in Apps.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends React.Component
+{
+  render()
+  {
+    return(
+      <div>
+        <p> Hey</p>
+      </div>
+      );
+  }
+}
+
+export default App;
+
